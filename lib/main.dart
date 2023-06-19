49879a722af31bc40da0e15c89f24bc4478775a3
Samindu-Denuwan <saminduloyola9280@gmail.com>
2023-06-19 11:43:12 +0530

    MERGING ALL BRANCHES

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:windy/screens/home_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+        systemNavigationBarColor:Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark));
+
+
+  }
   runApp(const MyApp());
 }
 
